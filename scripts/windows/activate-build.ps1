4817170ea2d2c7eeaec3d8f5946d2a4b07a5daa4
scopogger <scopogger@gmail.com>
2026-05-22 07:35:24 +0500

    upgraded mainly scripts

--- scripts/windows/activate-build.ps1
+++
@@ -1,7 +0,0 @@
-# activate-build.ps1
-
-$ErrorActionPreference = 'Stop'
-Set-StrictMode -Version Latest
-
-# Activate the build virtual environment
-.\.venv-build\Scripts\Activate.ps1
--- scripts/windows/activate-dev.ps1
+++
@@ -1,7 +0,0 @@
-# activate_dev_win.ps1
-
-$ErrorActionPreference = 'Stop'
-Set-StrictMode -Version Latest
-
-# Activate the dev virtual environment
-.\.venv-dev\Scripts\Activate.ps1
